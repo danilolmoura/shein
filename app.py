0bf74558f6a4f6b2e72e7bbb9c32b55e8fca7af6
Danilo Moura <danilolmoura@gmail.com>
2024-12-12 19:12:05 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/produtos")
+def produtos():
+    produtos = [
+        {
+            "nome": "vestido",
+            "preco": 79.99,
+            "categoria": "roupas",
+
+        },
+        {
+            "nome": "Havaianas",
+            "preco": 49.99,
+            "categoria": "calçados",
+        },
+        {
+            "nome": "Copo",
+            "preco": 2.99,
+            "categoria": "utensílios",
+        }
+    ]
+
+    return render_template('produtos.html', produtos = produtos)
+
+
+if __name__ == '__main__':
+    app.run()
