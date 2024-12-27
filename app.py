ddbda356fbd1c928868d4d8f7d5141c7b4a44505
Danilo Moura <danilolmoura@gmail.com>
2024-12-27 15:42:54 -0300

    adiciona tela de cadastro de produtos

--- app.py
+++ app.py
@@ -1,52 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    categorias = {
-        "roupas": "Roupas",
-        "feminino": "Feminino",
-        "plus_size": "Plus Size",
-        "masculino": "Masculino",
-        "bebe_maternidade": "Bebê & Maternidade",
-        "sapatos_bolsas": "Sapatos e Bolsa",
-    }
+# Configura banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shein.db'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = True
+db = SQLAlchemy(app)
 
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(), nullable=False)
+    preco = db.Column(db.Float(), nullable=False)
+    categoria = db.Column(db.String(), nullable=False)
+    descricao = db.Column(db.String(), nullable=True)
+
+    def __repr__(self):
+        return f'<Produto {self.id} {self.nome}>'
+
+
+with app.app_context():
+    db.create_all()
+
+# Fim banco de dados
+
+
+categorias = {
+    "roupas": "Roupas",
+    "feminino": "Feminino",
+    "plus_size": "Plus Size",
+    "masculino": "Masculino",
+    "bebe_maternidade": "Bebê & Maternidade",
+    "sapatos_bolsas": "Sapatos e Bolsa",
+}
+
+
+@app.route("/")
+def index():
     return render_template('index.html', categorias=categorias)
+
+
+@app.route("/produtos/cadastrar", methods=['GET', 'POST'])
+def adicionar_produtos():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        preco = request.form['preco']
+        categoria = request.form['categoria']
+
+        produto = Produto(nome=nome, preco=preco, categoria=categoria)
+
+        db.session.add(produto)
+        db.session.commit()
+
+        return redirect(url_for('produtos'))
+
+    return render_template('produtos_cadastrar.html', categorias=categorias)
 
 
 @app.route("/produtos/", defaults={"categoria": None})
 @app.route("/produtos/<categoria>")
 def produtos(categoria):
-    produtos = [
-        {
-            "nome": "vestido",
-            "preco": 79.99,
-            "categoria": "roupas",
-        },
-        {
-            "nome": "Havaianas",
-            "preco": 49.99,
-            "categoria": "sapatos_bolsas",
-        },
-        {
-            "nome": "Copo",
-            "preco": 2.99,
-            "categoria": "utensilios",
-        },
-        {
-            "nome": "Camiseta",
-            "preco": 39.99,
-            "categoria": "feminino",
-        },
-        {
-            "nome": "Chupeta",
-            "preco": 29.99,
-            "categoria": "bebe_maternidade",
-        },
-    ]
+    produtos = Produto.query.all()
 
     if categoria != None:
         itens_filtrados = []
@@ -81,7 +96,3 @@ def produtos(categoria):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
